34338a042faa419f40017eb7f8230d08fe37ddc0
Omidreza <omd.reza.masoumi@gmail.com>
2024-12-17 20:54:57 +0330

    removed backend

--- phase1/backend/routes/categoriesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCategories } = require('../controllers/categoriesController');
-const router = express.Router();
-
-// Route to get categories
-router.get('/', getCategories);
-
-module.exports = router;
--- phase1/backend/routes/scoreboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getScoreboard } = require('../controllers/scoreboardController');
-
-const router = express.Router();
-
-// Route to get the scoreboard
-router.get('/', getScoreboard);
-
-module.exports = router;
