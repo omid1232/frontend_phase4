88b0db2f4a0633fd1500033d0620df2c9d2ae323
Omidreza <omd.reza.masoumi@gmail.com>
2025-01-29 12:22:42 +0330

    scoreboard api call, scoreboard nav bar

--- phase1/myproject/src/pages/ShowScoreboard.js
+++ phase1/myproject/src/pages/ShowScoreboard.js
@@ -3,11 +3,12 @@ import './scoreboard.css';
 
 const Scoreboard = () => {
   const [players, setPlayers] = useState([]);
+  console.log('players ->', players)
 
   useEffect(() => {
     const fetchTopPlayers = async () => {
       try {
-        const response = await fetch('http://localhost:8080/api/scoreboard');
+        const response = await fetch('http://localhost:8080/api/players');
         const data = await response.json();
         setPlayers(data);
       } catch (error) {
@@ -20,6 +21,14 @@ const Scoreboard = () => {
 
   return (
     <div className="scoreboard-container">
+      <nav className="vertical-menu">
+        <a href="/player">Home</a>
+        <a href="/play_game">Play Game</a>
+        <a href="/scoreboard">Scoreboard</a>
+        <div className="logout-btn">
+          <a href="/">Logout</a>
+        </div>
+      </nav>
       <h2>Top Players</h2>
       <table className="score-table">
         <thead>
